c1aaad10a2fc2a461dd130cb82a5a02dadccc4bf
Evan McConnell <mcconnell.evan@gmail.com>
2019-09-19 14:12:38 +0100

    second class 19/09/09

---
+++ GameDevPracticum/Assets/scripts/Timer.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    // Use this for initialization
+    float timeLimit = 120;
+    float time = 0.0f;
+    string seconds, minutes;
+    void Start () {
+
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        time += Time.deltaTime;
+		print(time);
+        seconds = (time%60).ToString("f2");
+		//seconds = Convert.ToString();
+        minutes = ((int)time/60).ToString();
+
+        
+        print(minutes+":"+seconds);
+        GameObject.Find("Clock").GetComponent<Text>().text = minutes + ":" + seconds;
+        if (time > (timeLimit - 2))
+        {
+                //GameObject.Find("userMessage").GetComponent<Text>().text ="Time UP!";
+
+
+        }
+        if (time > timeLimit)
+        {
+            SceneManager.LoadScene("level1");
+
+        }
+        
+        
+    }
+}
