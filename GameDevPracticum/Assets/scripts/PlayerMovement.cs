4b555ca851cf06fac9e8a43137cc17393c2a6054
Evan McConnell <mcconnell.evan@gmail.com>
2019-09-30 12:13:19 +0100

    Player Movement Start

---
+++ GameDevPracticum/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 4;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0, vertical);
+        rb.AddForce(movement * speed / Time.deltaTime);
+    }
+}
